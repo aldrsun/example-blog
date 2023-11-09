8e52c9fefa4a6af1361f7ebc85a6a173ca6e0a82
aldrsun <ali.dursun@metu.edu.tr>
2023-11-09 11:01:30 +0300

    like system's codes has been optimized

--- resources/views/blog/posts/like.blade.php
+++ resources/views/blog/posts/like.blade.php
@@ -1,33 +1,23 @@
-
-
 <div>
     <div style="float: right">
     {{$post->likes->count()}}
     </div>
     <div>
         @auth
-            @if($post->likes->contains('user', auth()->user()))
-                <form class="like-form" action="{{route('post.like.destroy', [
-                        'post' => $post->slug,
-                        'like' => $post->likes->where('user_id', auth()->user()->id)->first()->id
-                        ])}}" method="POST">
-                    @method('DELETE')
-                    @csrf
-                    <img class="heart-filled-image" style="float: right; max-width: 14%; height: auto;"
-                         src="{{asset('assets/heart-filled.png')}}"
-                         alt="{{__('like')}}"/>
-                </form>
-            @else
-                <form class="like-form" action="{{route('post.like.store', [
-                    'post'      =>  $post->slug,
+            @php( $postLiked = $post->likes->contains('user_id', auth()->user()->id))
+            <form class="like-form" action="{{route('post.like'.(($postLiked) ? '.destroy' : '.store'), [
+                    'post' => $post->slug,
+                    'like' => ($postLiked) ? $post->likes->where('user_id', auth()->user()->id)->first()->id : 0,
                     'post_id'   =>  $post->id,
                     'user_id'   =>  auth()->user()->id])}}" method="POST">
-                    @csrf
-                    <img class="heart-outline-image" style="float: right; max-width: 14%; height: auto;"
-                         src="{{asset('assets/heart-outline.png')}}"
-                         alt="{{__('like')}}"/>
-                </form>
-            @endif
+                @if($postLiked)
+                    @method('DELETE')
+                @endif
+                @csrf
+                <img class="heart-{{($postLiked) ? "filled" : "outline"}}-image" style="float: right; max-width: 14%; height: auto;"
+                     src="{{asset('assets/heart-'.(($postLiked) ? 'filled' : 'outline').'.png')}}"
+                     alt="{{__('like')}}"/>
+            </form>
         @else
             <form class="like-form-unauth" action="{{route('login')}}" method="GET">
                 @csrf
